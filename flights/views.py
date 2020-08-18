c45ca2e76f0b846cf25f5815f8f09325e0e1cc61
isgm255 <khinthet@isgm2.com>
2020-08-18 13:07:32 +0630

    add models, initial template, render html page

--- flights/views.py
+++ flights/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
-
+from .models import Flight
 def index(request):
-    return HttpResponse("Hello, World! How are you?")
+    contex = {
+        "flights": Flight.objects.all()
+    }
+    print(contex)
+    return render(request, 'flights/index.html', contex)
