6d0327be941b844cfd40a17b6615c4791ea7e673
isgm255 <khinthet@isgm2.com>
2020-08-19 14:08:11 +0630

    Add : add parameter to browser's url

--- flights/views.py
+++ flights/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 # Create your views here.
 from .models import Flight
 def index(request):
@@ -9,3 +9,9 @@ def index(request):
     }
     print(contex)
     return render(request, 'flights/index.html', contex)
+
+def flight(request, flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExit:
+        raise Http404("Flight does not exit")
